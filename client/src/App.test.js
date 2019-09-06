bd8a9169280b0ccb0f8ccd276dcafd7a9265a269
Diana Grams <deegrams221@gmail.com>
2019-09-06 13:27:23 -0600

    had to re-add @testing-library/react and jest-dom, tests passing now

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {render} from '@testing-library/react';
+import '@testing-library/react';
 import 'jest-dom/extend-expect';
 import App from './App';
 import PlayerList from './Components/PlayerList';
