5ab4d9132a42d1bf02900b1462a2fc7a96179457
Diana Grams <deegrams221@gmail.com>
2019-09-06 12:35:03 -0600

    fixed mapping, styling, and useLocalStorage -mvp

--- client/src/Components/PlayerList.js
+++ client/src/Components/PlayerList.js
@@ -2,6 +2,7 @@ import React from 'react';
 import '../App.css';
 
 export default function PlayerList(props) {
+    console.log(props);
   return (
     <div className="player-list">
       <ul>
--- client/src/Components/useLocalStorage.js
+++ client/src/Components/useLocalStorage.js
@@ -1,13 +1,15 @@
 import {useState} from 'react';
 
-export const useLocalStorage = (key, initialValue) => {
-  const item = JSON.parse(localStorage.getItem(key));
-
-  const [storedValue, setStoredValue] = useState(item || initialValue);
-
-  const setValue = (value) => {
-    window.localStorage.setItem(key, JSON.stringify(value));
+const useLocalStorage = (key, initialValue) => {
+  const [storedValue, setStoredValue] = useState(() => {
+    const item = window.localStorage.getItem(key);
+    return item ? JSON.parse(item) : initialValue;
+  });
+  const setValue = value => {
     setStoredValue(value);
-  }
+    window.localStorage.setItem(key, JSON.stringify(value));
+  };
   return [storedValue, setValue];
-}
+};
+
+export default useLocalStorage;
